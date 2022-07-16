c7450783a319d2b58a1415505db56c0f0d7051c1
Hazem Essam <hzmessam.dev@outlook.com>
2022-07-16 10:37:37 +0200

    Refactor code

--- server.py
+++ server.py
@@ -1,62 +1,56 @@
-from flask import Flask, render_template
+from __future__ import annotations
 import os
+from dataclasses import dataclass
+from typing import List
+from flask import Flask, render_template
 
 
+MEDIA_DIR: str = 'public/media'
+FILES_BLACKLIST: List[str] = ['.gitkeep', 'desktop.ini']
+
+
+@dataclass
 class File:
-    def __init__(self, filename, type):
-        self.filename = filename
-        self.link = None
-        self.type = type
+    filename: str
+    type: str
+    link: str = None
 
-    def generate_link(self):
-        filename = self.filename.replace(' ', '%20')
-        self.link = f'/public/media/{self.type}s/{filename}'
+    def generate_link(self) -> File:
+        filename: str = self.filename.replace(' ', '%20')
+        self.link = f'{MEDIA_DIR}/{self.type}s/{filename}'
+        return self
+
+    @classmethod
+    def get_files(cls, dir_path: str, type: str) -> List[File]:
+        filenames: List[str] = [filename for filename in os.listdir(dir_path) if filename not in FILES_BLACKLIST]
+        return [File(filename, type).generate_link() for filename in filenames]
 
 
-app = Flask(
-    __name__,
-    static_folder='public',
-    static_url_path='/public'
-)
+app = Flask(__name__, static_folder='public', static_url_path='/public')
+
 
 @app.route('/')
 def get_index():
     return render_template('index.html')
 
+
 @app.route('/docs')
 def get_docs():
-    docs_names = os.listdir('public/media/docs')
-    docs_names.remove('.gitkeep')
-    files = list()
-    for name in docs_names:
-        file = File(filename=name, type='doc')
-        file.generate_link()
-        files.append(file)
+    files: List[File] = File.get_files(f'{MEDIA_DIR}/docs', 'doc')
     return render_template('links.html', files=files)
+
 
 @app.route('/imgs')
 def get_imgs():
-    imgs_names = os.listdir('public/media/imgs')
-    imgs_names.remove('.gitkeep')
-    files = list()
-    for name in imgs_names:
-        file = File(filename=name, type='img')
-        file.generate_link()
-        files.append(file)
+    files: List[File] = File.get_files(f'{MEDIA_DIR}/imgs', 'img')
     return render_template('links.html', files=files)
+
 
 @app.route('/vids')
 def get_vids():
-    vids_names = os.listdir('public/media/vids')
-    vids_names.remove('.gitkeep')
-    files = list()
-    for name in vids_names:
-        file = File(filename=name, type='vid')
-        file.generate_link()
-        files.append(file)
+    files: List[File] = File.get_files(f'{MEDIA_DIR}/vids', 'vid')
     return render_template('links.html', files=files)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=2121, debug=True)
-
